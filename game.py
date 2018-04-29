f297f8f952be94a5bed4c22d6d126cc72f044924
explosion33 <38818379+explosion33@users.noreply.github.com>
2018-04-29 14:00:28 -0700

    improved graphics by clearing the screen and reprinting the new board
    fixed errors when detecting if the guess was valid

--- game.py
+++ game.py
@@ -1,44 +1,53 @@
 import board
+import os
 from random import randint
 i = 0
-turns = 10
+error = ""
+turns = 8
 offset = 0
-
+unused_variable = os.system("cls")
 board.printBoard()
 ship = board.genShip(randint(0,4), randint(0,4))
 
 while i < turns:
+    print(error)
     print("Turn " + str(i + 1))
     print()
     while True:
         try:
-            guessY = int(input("guess Y: "))
-            guessX = int(input("guess X: "))
+            guessY = int(input("guess X: "))
+            guessX = int(input("guess Y: "))
         except ValueError:
-            print()
+            unused_variable = os.system("cls")
+            board.printBoard()
             print("Thats not even a number")
+            print("Turn " + str(i+1))
             print()
         else:
             break
         
-    guess = board.fire(guessX-1, guessY-1)
-
+    
+    if guessY > 5 or guessX > 5 or guessY <= 0 or guessX <= 0:
+        error = "You must guess within the ocean"
+        i -= 1
+        guess = False
+    else:
+        guess = board.fire(guessX-1, guessY-1)
     if guess == True:
-        print()
-        print("You already guessed that")
-        print()
+        error = "You already guessed that"
         i -= 1
 
-    if guessY > 4 or guessX > 4:
-        print()
-        print("You must guess within the ocean")
-        i -= 1
-
+    
     if guess == ship:
+        unused_variable = os.system("cls")
         print("You Win")
         break
-    print()
+    unused_variable = os.system("cls")
     board.printBoard()
     i += 1
 else:
     print("You Lose")
+    print()
+    print("Ship Location:")
+    print("     X: " + str(int(ship[0]) + 1))
+    print("     Y: " + str(int(ship[2]) + 1))
